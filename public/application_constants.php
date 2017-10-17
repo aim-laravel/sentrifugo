d7bfb3bcb5bcc78c29442997735331ca7a300e3d
Lakshay Verma <lakshay.verma@walkwel.in>
2017-10-17 18:19:26 +0530

    Working Fine

--- public/application_constants.php
+++ public/application_constants.php
@@ -1,4 +1,4 @@
 <?php
-       defined('SUPERADMIN_EMAIL') || define('SUPERADMIN_EMAIL','');
+       defined('SUPERADMIN_EMAIL') || define('SUPERADMIN_EMAIL','lakshay.verma@walkwel.in');
        defined('APPLICATION_NAME') || define('APPLICATION_NAME','Sentrifugo');
      ?>
---
+++ public/mail_settings_constants.php
@@ -0,0 +1,8 @@
+<?php
+	           defined('MAIL_SMTP') || define('MAIL_SMTP','mail.walkwel.in');
+	           defined('MAIL_USERNAME') || define('MAIL_USERNAME','lakshay.verma@walkwel.in');
+	           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','traverseWel189');
+	           defined('MAIL_PORT') || define('MAIL_PORT','25');
+	           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
+	           defined('MAIL_TLS') || define('MAIL_TLS','');
+	         ?>
